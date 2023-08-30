0894dea57133b1f788153f3aaf6e831568cf1a32
shefali12-ab <kanaujiashefali@gmail.com>
2023-08-30 21:10:17 +0530

    added Cityname

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,8 +1,8 @@
 import react from "react"
 const Card2 =(props)=>{
     return(<>
-    <div className="bg-midnight w-[19rem] h-[13rem] mt-[4rem] ml-[11rem] p-2 inline-block text-white font-raleway ">
-        <h2 className="text-center p-4 text-base mb-[2rem]" >{props.text}</h2>
+    <div className="bg-midnight w-[19rem] h-[11rem] mt-[2rem] ml-[11rem] p-2 inline-block text-white font-raleway ">
+        <h2 className="text-center p-4 text-base mb-[1rem]" >{props.text}</h2>
         <p className="text-center text-5xl">{props.val}  <span className="text-3xl">  {props.unit}</span> </p>
     </div>
     
--- src/Components/Card3.jsx
+++ src/Components/Card3.jsx
@@ -2,8 +2,8 @@ import react from "react"
 const Card3 =(props)=>{
     return(<>
     <div className="bg-midnight w-[19rem] h-[10rem] mt-[4rem] ml-[11rem] p-2 inline-block text-white font-raelway">
-        <h2 className="text-center p-4 text-base">{props.text}</h2>
-        <p className="text-center text-5xl" >{props.val} <span className="text-3xl">{props.unit}</span></p>
+        <h2 className="text-center p-4 text-base mb-[1rem]">{props.text}</h2>
+        <p className="text-center text-4xl" >{props.val} <span className="text-3xl">{props.unit}</span></p>
     </div>
     
     </>)
